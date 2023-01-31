fbdb3b244b226f8c8dac5d8481da8d0c66455108
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-01-31 16:33:40 -0400

    Config controllers

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -1,6 +1,48 @@
 const Picture = require("../models/Picture")
+const fs = require("fs")
 
 
-exports.create = async (req, res) => [
-    res.json("Deu certo!")
-]
+exports.create = async (req, res) => {
+    try {
+        const {name} = req.body
+        const file = req.file
+        const picture = new Picture({
+            name,
+            src: file.path,
+        })
+        await picture.save()
+        res.json({picture, msg: "Imagem salva com sucesso!"})
+    } catch (error) {
+        res.status(500).json({message: "Erro ao salvar a imagem"})
+    }
+}
+
+
+    exports.findAll = async (req, res) => {
+        try {
+            const pictures = await Picture.find();
+            res.json(pictures);
+        } catch (err) {
+            res.status(500).json({ message: "Erro ao buscar as imagens." });
+        }
+}
+
+
+    exports.remove = async(req, res) => {
+        try {
+            const picture = await Picture.findById(req.params.id)
+
+            if(!picture){
+                return res.status(404).json({message: "Imagem não encontrada"})
+            }
+
+            fs.unlinkSync(picture.src)
+
+            await picture.remove()
+
+            res.json({message: "Imagem removida com sucesso!"})
+
+        } catch (error) {
+            res.status(500).json({message: "Erro ao excluir a imagem"})
+        }
+    }
--- routes/picture.js
+++ routes/picture.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const upload = require("../config/multer");
+const PictureController = require("../controllers/pictureController");
 
-const PictureController = require("../controllers/pictureController")
+router.post("/", upload.single("file"), PictureController.create);
+router.get("/", PictureController.findAll);
+router.delete("/:id", PictureController.remove)
 
-router.post("/", PictureController.create);
-
-module.exports = router
+module.exports = router;
