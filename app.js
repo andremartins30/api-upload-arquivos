251c4f6329fc98ddd5bf64bbc1ad22bea48d0ff0
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-01-31 14:13:54 -0400

    Add routes, config Multer

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ require("./db")
 
 const port = process.env.PORT || 4000;
 
+const pictureRouter = require("./routes/picture")
+
+app.use("/pictures", pictureRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
---
+++ controllers/pictureController.js
@@ -0,0 +1,6 @@
+const Picture = require("../models/Picture")
+
+
+exports.create = async (req, res) => [
+    res.json("Deu certo!")
+]
